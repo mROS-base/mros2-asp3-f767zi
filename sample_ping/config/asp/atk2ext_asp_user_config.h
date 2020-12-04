9c8b6467a6cc71aad7ec31b835076952e6bad258
ryoto.takashima <ryoto.takashima>
2020-12-04 16:56:21 +0900

    add:sample_ping

---
+++ sample_ping/config/asp/atk2ext_asp_user_config.h
@@ -0,0 +1,10 @@
+#ifndef _ATK2EXT_ASP_USER_CONFIG_H_
+#define _ATK2EXT_ASP_USER_CONFIG_H_
+
+#include "atk2ext_asp_config.h"
+
+#define ATK2EXT_ASP_FLAG_USER_NUM	(3U)
+
+extern const Atk2ExtFlagMapEntryType atk2ext_flag_user_map[ATK2EXT_ASP_FLAG_USER_NUM];
+
+#endif /* _ATK2EXT_ASP_USER_CONFIG_H_ */
---
+++ sample_ping/sample_ping.c
@@ -0,0 +1,43 @@
+#include "atk2ext_common.h"
+#include "cmsis_os.h"
+#include "atk2ext_sys_config.h"
+#include "sample_ping.h"
+
+TASK(Atk2ExtTestTask)
+{
+	UserThreadAtk2TaskMapEntryType config;
+	StatusType ercd = Atk2TaskConfigGet(&config);
+
+	if (ercd == E_OK) {
+		config.func(config.argument);
+	}
+
+	(void)osThreadTerminate(NULL);
+	return;
+}
+
+TASK(Atk2ExtTest2Task)
+{
+	UserThreadAtk2TaskMapEntryType config;
+	StatusType ercd = Atk2TaskConfigGet(&config);
+
+	if (ercd == E_OK) {
+		config.func(config.argument);
+	}
+
+	(void)osThreadTerminate(NULL);
+	return;
+}
+
+TASK(Atk2ExtTest3Task)
+{
+	UserThreadAtk2TaskMapEntryType config;
+	StatusType ercd = Atk2TaskConfigGet(&config);
+
+	if (ercd == E_OK) {
+		config.func(config.argument);
+	}
+
+	(void)osThreadTerminate(NULL);
+	return;
+}
