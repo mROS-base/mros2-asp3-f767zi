269ace9b02d0156dbe1eaf15a3b095d30e5302e9
ken551 <hide.delorean@gmail.com>
2020-12-07 14:06:16 +0900

    clean: cleaneed sample1.c code

--- samples/udp/build/sample1.c
+++ samples/udp/build/sample1.c
@@ -12,7 +12,7 @@ UART_HandleTypeDef huart3;
 ETH_HandleTypeDef heth;
 
 /*
- *  �����ӥ�������Υ��顼�Υ�������
+ *  サービスコールのエラーのログ出力
  */
 Inline void
 svc_perror(const char *file, int_t line, const char *expr, ER ercd)
@@ -26,13 +26,13 @@ svc_perror(const char *file, int_t line, const char *expr, ER ercd)
 
 
 /*
- *  �ᥤ�󥿥���
+ *  メインタスク
  */
 
 void udp_receive_callback(void *arg, struct udp_pcb *upcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
 {
 	syslog(LOG_NOTICE,"recv: %s", p->payload);
-   pbuf_free(p);		// Free the p buffer
+	pbuf_free(p);		// Free the p buffer
 
    //udp_transmit(upcb, addr, udp2_tx_data, udp2_tx_len);
 }
@@ -47,11 +47,11 @@ void main_task(intptr_t exinf)
 	ena_int(77);
 	dev_timer_init();
 	/*
-	 *  ���ꥢ��ݡ��Ȥν����
+	 *  シリアルポートの初期化
 	 *
-	 *  �����ƥ����������Ʊ������?��ݡ��Ȥ�Ȥ����ʤɡ����ꥢ��
-	 *  �ݡ��Ȥ������ץ�Ѥߤξ��ˤϤ�����?E_OBJ���顼�ˤʤ뤬���پ��?
-	 *  �ʤ���
+	 *  システムログタスクと同じシリアルポートを使う場合など，シリアル
+	 *  ポートがオープン済みの場合にはここでE_OBJエラーになるが，支障は
+	 *  ない．
 	 */
 	/*
 	ercd = serial_opn_por(TASK_PORTID);
@@ -60,37 +60,37 @@ void main_task(intptr_t exinf)
 									itron_strerror(ercd), SERCD(ercd));
 	}
 	*/
-	  /* USER CODE BEGIN 5 */
-	  struct udp_pcb *pcb;
-	  struct pbuf *p;
-	  err_t err;
-	  ip4_addr_t dst_addr;
-	  const unsigned short src_port = 12345;
-	  const unsigned short dst_port = 8080;
+	struct udp_pcb *pcb;
+	struct pbuf *p;
+	err_t err;
+	ip4_addr_t dst_addr;
+	const unsigned short src_port = 12345;
+	const unsigned short dst_port = 8080;
 
-	  IP4_ADDR(&dst_addr,192,168,11,3);
+	IP4_ADDR(&dst_addr,192,168,11,3);
 
-	  pcb = udp_new();
-	  err = udp_bind(pcb, IP_ADDR_ANY, src_port);
+	pcb = udp_new();
+	err = udp_bind(pcb, IP_ADDR_ANY, src_port);
 
-	  uint32_t cnt = 0;
+	uint32_t cnt = 0;
 
-	     if (pcb)
-	     {
-	         err = udp_bind(pcb, IP_ADDR_ANY, 1234);
+	if (pcb)
+	{
+		err = udp_bind(pcb, IP_ADDR_ANY, 1234);
 
-	         if(err == ERR_OK)
-	         {
-	            udp_recv(pcb, udp_receive_callback, NULL);
-	         }
-	     }
+		if(err == ERR_OK)
+		{
+		udp_recv(pcb, udp_receive_callback, NULL);
+		}
+	}
 
 	while(true){
+		syslog(LOG_NOTICE, "send udp packet");
 		p = pbuf_alloc(PBUF_TRANSPORT, sizeof(4), PBUF_RAM);
-			    *(uint32_t *)p->payload = cnt++;
-			    p->len = 4;
-			    err = udp_sendto(pcb, p, &dst_addr, dst_port);
-			    pbuf_free(p);
+		*(uint32_t *)p->payload = cnt++;
+		p->len = 4;
+		err = udp_sendto(pcb, p, &dst_addr, dst_port);
+		pbuf_free(p);
 		tslp_tsk(100);
 	}
 	SVC_PERROR(serial_ctl_por(TASK_PORTID,
