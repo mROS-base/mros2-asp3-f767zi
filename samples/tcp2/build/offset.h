39c306dcbaac85b863fc45170fa9e6f3a722dd73
ryoto.takashima <ryoto.takashima>
2020-12-08 15:54:10 +0900

    add:tcp test2 app

---
+++ samples/tcp2/build/offset.h
@@ -0,0 +1,11 @@
+/* offset.h */
+
+#define TCB_p_tinib	8
+#define TCB_texptn	16
+#define TCB_sp	24
+#define TCB_pc	28
+#define TINIB_exinf	4
+#define TINIB_task	8
+#define TCB_enatex	14
+#define TCB_enatex_bit	2
+#define TCB_enatex_mask	0x4
