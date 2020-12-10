c2775aa3b2af20a6288ee09364301c4583b20aa2
ryoto.takashima <ryoto.takashima>
2020-12-10 17:34:09 +0900

    edit:lwip change to UTF-8

--- lwip/src/apps/sntp/sntp.c
+++ lwip/src/apps/sntp/sntp.c
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (c) 2007-2009 Frédéric Bernon, Simon Goldschmidt
+ * Copyright (c) 2007-2009 Fr辿d辿ric Bernon, Simon Goldschmidt
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -31,7 +31,7 @@
  *
  * This file is part of the lwIP TCP/IP stack.
  *
- * Author: Frédéric Bernon, Simon Goldschmidt
+ * Author: Fr辿d辿ric Bernon, Simon Goldschmidt
  */
 
 
