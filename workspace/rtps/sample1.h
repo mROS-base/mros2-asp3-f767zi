39dc3362bfc2271b2f7feebd0263752849617bbd
ken551 <hide.delorean@gmail.com>
2020-12-20 13:18:30 +0900

    edit: edited sample1.h and .cfg

--- workspace/rtps/sample1.h
+++ workspace/rtps/sample1.h
@@ -80,17 +80,18 @@
  *  関数のプロトタイプ宣言
  */
 #ifndef TOPPERS_MACRO_ONLY
-
+#ifdef __cplusplus
+extern "C" {
+#endif
+void main_task(void);
+#ifdef __cplusplus
+}
+#endif
 extern void	task(intptr_t exinf);
-extern void	main_task(intptr_t exinf);
 extern void	tex_routine(TEXPTN texptn, intptr_t exinf);
 #ifdef CPUEXC1
 extern void	cpuexc_handler(void *p_excinf);
 #endif /* CPUEXC1 */
 extern void	cyclic_handler(intptr_t exinf);
-extern void	alarm_handler(intptr_t exinf);
-void  dev_timer_init( void );
-unsigned short int  dev_timer_clr_int( void );
-void  main_timer_handler(void *p_excinf);
 void Error_Handler();
 #endif /* TOPPERS_MACRO_ONLY */
