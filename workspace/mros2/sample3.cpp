07a59446099d005c1edb7afd41026e55c7626ba3
ken551 <hide.delorean@gmail.com>
2021-01-14 16:30:54 +0900

    edit: cleaned sample3.cpp

--- workspace/mros2/sample3.cpp
+++ workspace/mros2/sample3.cpp
@@ -44,65 +44,10 @@ extern "C" void callHbSubFunc(void *arg)
 
 void main_task(void)
 {
-  MX_LWIP_Init();
-
-	startRTPStest();
-
-  BSP_LED_On(LED1);
-  ext_tsk();
-}
-
-//Callback function to set the boolean to true upon a match
-void setTrue(void* args){
-	*static_cast<volatile bool*>(args) = true;
-}
-
-void message_callback(void* callee, const rtps::ReaderCacheChange& cacheChange){
-	rtps::Writer* writer = (rtps::Writer*) callee;
-	static std::array<uint8_t,10> data{};
-	data.fill(10);
-	auto* change = writer->newChange(rtps::ChangeKind_t::ALIVE, data.data(), data.size());
-}
-
-//Function to start the RTPS Test
-void startRTPStest(){
-	mros2::init(NULL,NULL);
-
-	// //Initialize variables and complete RTPS initialization
-	// bool subMatched = false;
-	// bool pubMatched = false;
-	// bool received_message = false;
-
-	// static rtps::Domain domain;
-
-	// //Create RTPS participant
-	// rtps::Participant* part = domain.createParticipant();
-	// if(part == nullptr){
-	// 	return;
-	// }
-
-	// //Register callback to ensure that a publisher is matched to the writer before sending messages
-	// part->registerOnNewPublisherMatchedCallback(setTrue, &pubMatched);
-	// part->registerOnNewSubscriberMatchedCallback(setTrue, &subMatched);
-
-	// //Create new writer to send messages
-	// rtps::Writer* writer = domain.createWriter(*part, "TOLINUX","TEST", false);
-	// rtps::Reader* reader = domain.createReader(*part, "TOSTM",  "TEST", false);
-	// reader->registerCallback(&message_callback, writer);
-
-	// domain.completeInit();
-
-	// //Check that writer creation was successful
-	// if(writer == nullptr || reader == nullptr){
-	// 	return;
-	// }
-
-	// //Wait for the subscriber on the Linux side to match
-	// while(!subMatched || !pubMatched){
-    // tslp_tsk(1000000);
-    // BSP_LED_Toggle(LED3);
-	// }
-
+	MX_LWIP_Init();
+	mros2::init(NULL, NULL);
+	BSP_LED_On(LED1);
+	ext_tsk();
 }
 
 void
