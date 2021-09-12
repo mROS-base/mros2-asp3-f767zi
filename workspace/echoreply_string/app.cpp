442fd2037abda0bafbb7bdbc1b1fea608f55f95a
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-09-12 20:27:37 +0900

    fix arguments of mros2 APIs to follow https://github.com/mROS-base/mros2/pull/10

--- workspace/echoreply_string/app.cpp
+++ workspace/echoreply_string/app.cpp
@@ -14,22 +14,27 @@ void userCallback(std_msgs::msg::String *msg)
   pub.publish(*msg);
 }
 
-void main_task(void)
+int main(int argc, char * argv[])
 {
   MROS2_INFO("mROS 2 application is started");
 
-  mros2::init(NULL, NULL);
+  mros2::init(argc, argv);
   MROS2_DEBUG("mROS 2 initialization is completed");
   BSP_LED_Toggle(LED1);
 
-  mros2::Node node = mros2::Node::create_node();
-  sub = node.create_subscription("to_stm", 1, userCallback);
-  pub = node.create_publisher<std_msgs::msg::String>("to_linux", NULL);
+  mros2::Node node = mros2::Node::create_node("mros2_node");
+  pub = node.create_publisher<std_msgs::msg::String>("to_linux", 10);
+  sub = node.create_subscription("to_stm", 10, userCallback);
   std_msgs::msg::String msg;
 
   MROS2_INFO("ready to pub/sub message");
   mros2::spin();
   BSP_LED_Toggle(LED3);
+}
+
+void main_task(void)
+{
+  main(0, NULL);
 }
 
 void
