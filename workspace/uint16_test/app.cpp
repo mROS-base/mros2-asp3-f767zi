7a69676162f301c2465cc7202ff30a4358b1a8c5
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-04 16:12:18 +0900

    app.cpp for uint16_test added

--- workspace/uint16_test/app.cpp
+++ workspace/uint16_test/app.cpp
@@ -9,8 +9,8 @@ mros2::Publisher pub;
 
 void userCallback(std_msgs::msg::UInt16 *msg)
 {
-  MROS2_INFO("subscribed msg: %d", msg->data);
-  MROS2_INFO("publishing msg: %d", msg->data);
+  MROS2_INFO("subscribed msg: %u", msg->data);
+  MROS2_INFO("publishing msg: %u", msg->data);
   pub.publish(*msg);
 }
 
