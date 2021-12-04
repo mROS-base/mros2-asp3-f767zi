57247a8f6745ca3b6ea26fd274a133e66c2d96b6
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-04 15:57:58 +0900

    app.cpp for uint16_test added

---
+++ workspace/uint16_test/config/autosar_os_ext_asp3_user_config.h
@@ -0,0 +1,10 @@
+#ifndef _AUTOSAR_OS_EXT_ASP3_USER_CONFIG_H_
+#define _AUTOSAR_OS_EXT_ASP3_USER_CONFIG_H_
+
+#include "cmsis_config_utl.h"
+
+#define AUTOSAR_OS_EXT_ASP3_FLAG_USER_NUM	(10U)
+
+extern const AutosarOsExtFlagMapEntryType autosar_os_ext_flag_user_map[AUTOSAR_OS_EXT_ASP3_FLAG_USER_NUM];
+
+#endif /* _AUTOSAR_OS_EXT_ASP3_USER_CONFIG_H_ */
