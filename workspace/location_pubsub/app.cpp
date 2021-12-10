1b7db4efcd63b60c5715c932e48cfd90569fbb44
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-10 09:19:09 +0900

    location_pubsub app created

---
+++ workspace/location_pubsub/app.cpp
@@ -0,0 +1,44 @@
+#include "app.h"
+#include "mros2.h"
+#include "location_msgs/msg/location.hpp"
+
+#include "stm32f7xx_nucleo_144.h"
+
+mros2::Subscriber sub;
+mros2::Publisher pub;
+
+void userCallback(location_msgs::msg::Location *msg)
+{
+  MROS2_INFO("subscribed msg: { x: %d, y: %d, z: %d }", msg->x, msg->y, msg->z);
+  MROS2_INFO("publishing msg: { x: %d, y: %d, z: %d }", msg->x, msg->y, msg->z);
+  pub.publish(*msg);
+}
+
+int main(int argc, char * argv[])
+{
+  MROS2_INFO("mROS 2 application is started");
+
+  mros2::init(argc, argv);
+  MROS2_DEBUG("mROS 2 initialization is completed");
+  BSP_LED_Toggle(LED1);
+
+  mros2::Node node = mros2::Node::create_node("mros2_node");
+  pub = node.create_publisher<location_msgs::msg::Location>("to_linux", 10);
+  sub = node.create_subscription("to_stm", 10, userCallback);
+  location_msgs::msg::Location msg;
+
+  MROS2_INFO("ready to pub/sub message");
+  mros2::spin();
+  BSP_LED_Toggle(LED3);
+}
+
+void main_task(void)
+{
+  main(0, NULL);
+}
+
+void
+led_cyclic_handler(intptr_t exinf)
+{
+  BSP_LED_Toggle(LED2);
+}
