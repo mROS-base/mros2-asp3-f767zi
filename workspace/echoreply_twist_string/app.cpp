efcf834b32ca4485cf70d464b6e0db8b1e27d68d
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-01-12 18:11:17 +0900

    able to make

--- workspace/echoreply_twist_string/app.cpp
+++ workspace/echoreply_twist_string/app.cpp
@@ -10,8 +10,6 @@ mros2::Publisher pub;
 
 void userCallback(std_msgs::msg::String *msg)
 {
-  
-  /*
   std::string sub_str1 = msg->data.substr(0,8);
   std::string sub_str2 = msg->data.substr(8,8);
   std::string sub_str3 = msg->data.substr(16,8);
@@ -19,26 +17,26 @@ void userCallback(std_msgs::msg::String *msg)
   std::string sub_str5 = msg->data.substr(32,8);
   std::string sub_str6 = msg->data.substr(40,8);
 
-  double linear_x = 0.000000; // std::stod(sub_str1);
-  double linear_y = 0.000002; // std::stod(sub_str2);
-  double linear_z = 0.000004; // std::stod(sub_str3);
-  double angluar_x = 0.000006; // std::stod(sub_str4);
-  double angluar_y = 0.000008; // std::stod(sub_str5);
-  double angluar_z = 0.000010; // std::stod(sub_str6);
+  double linear_x = std::stod(sub_str1);
+  double linear_y = std::stod(sub_str2);
+  double linear_z = std::stod(sub_str3);
+  double angular_x = std::stod(sub_str4);
+  double angular_y = std::stod(sub_str5);
+  double angular_z = std::stod(sub_str6);
   
   auto reconverted_message = std_msgs::msg::String();
 
-  std::string pub_str1 = "0.000000"; // std::to_string(linear_x);
-  std::string pub_str2 = "0.000002"; // std::to_string(linear_y);
-  std::string pub_str3 = "0.000004"; // std::to_string(linear_z);
-  std::string pub_str4 = "0.000006"; // std::to_string(angular_x);
-  std::string pub_str5 = "0.000008"; // std::to_string(angular_y);
-  std::string pub_str6 = "0.000010"; // std::to_string(angular_z);
+  std::string pub_str1 = std::to_string(linear_x);
+  std::string pub_str2 = std::to_string(linear_y);
+  std::string pub_str3 = std::to_string(linear_z);
+  std::string pub_str4 = std::to_string(angular_x);
+  std::string pub_str5 = std::to_string(angular_y);
+  std::string pub_str6 = std::to_string(angular_z);
 
-  std::string pub_str = "0.0000000.0000020.0000040.0000060.0000080.000010"; // pub_str1 + pub_str2 + pub_str3 + pub_str4 + pub_str5 + pub_str6;
+  std::string pub_str = pub_str1 + pub_str2 + pub_str3 + pub_str4 + pub_str5 + pub_str6;
   reconverted_message.data = pub_str;
-  */
-  pub.publish(*msg); //reconverted_message);
+
+  pub.publish(reconverted_message);
 }
 
 int main(int argc, char * argv[])
